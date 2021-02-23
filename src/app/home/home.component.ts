4e23dbc5af589c220453793d647bc00a0989d33f
viniciusad <viniciusad@outlook.com.br>
2021-02-22 21:33:23 -0300

    change: home component and auth service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/internal/operators/take';
+import { Login } from '../login.interface';
+import { AuthService } from '../shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  login: Login = {
+    senha: '123456',
+    usuario: 'helloWorld',
+  }
+
+  constructor(
+    private auth: AuthService,
+  ) { }
 
   ngOnInit(): void {
+    this.initialLogin();
+ }
+  initialLogin() {
+    this.auth.postLogin(this.login)
+    .pipe(
+      take(1),
+    ).subscribe(
+        (response) => { this.onSuccess(response) },
+        (error) => { this.onError(error) }
+      )
+  }
+  onSuccess(response: any) {
+    console.log(response)
+  }
+  onError(error: any) {
+      console.log(error);
   }
 
 }
--- src/app/shared/services/auth/auth.service.ts
+++ src/app/shared/services/auth/auth.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Login } from 'src/app/login.interface';
+import { Usuario } from 'src/app/usuario.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,19 +10,35 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   token: any;
+  usuario: any;
+
+  FAKE_BANK_API_URL = environment.FAKE_BANK_API_URL;
+
+  login: Login = {
+    senha: '123456',
+    usuario: 'helloWorld',
+  }
+
+  fixeds = {
+    senha: '',
+    usuario: '',
+  } ;
   
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+  ) { }
 
 
-  setUser(usuario: any) {
+  setUser(usuario: Usuario) {
+    this.usuario = usuario;
     localStorage.setItem('token', JSON.stringify(''));
   }
 
   getUser() {
-    if (this.token) {
-      return this.token;
+    if (this.usuario) {
+      return this.usuario;
     }
-    
+        
     const loggedUser = localStorage.getItem('token');
     if (loggedUser) {
       this.token = JSON.parse(loggedUser);
@@ -26,6 +46,32 @@ export class AuthService {
     }
 
     return null;
+  }
+
+  getToken() {
+    if(this.token) {
+      return this.token;
+    }
+
+    const savedToken = localStorage.getItem('token');
+    if(savedToken) {
+      this.token = savedToken;
+      return this.token;
+    }
+
+    return null;
+  }
+
+  estaLogado(): boolean {
+    //return this.getUsuario() && this.getToken.Token() ? true : false;
+    if (this.getUser() && this.getToken()) {
+      return true;
+    }
+    return false;
+  }
+
+  postLogin(login: Login) {
+    return this.httpClient.post<Login>(this.FAKE_BANK_API_URL + '/login', login);
   }
  
 }
