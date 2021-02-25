5194dcdae406bcce9fb7f67f837466a037ca03df
Renata <rmkarato@gmail.com>
2021-02-24 21:06:51 -0300

    new changes home

---
+++ src/app/shared/components/home5/home5.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home5',
+  templateUrl: './home5.component.html',
+  styleUrls: ['./home5.component.scss']
+})
+export class Home5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
